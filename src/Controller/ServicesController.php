9e19f6f4f09212536848559ae3c0695cf64d1ac3
Jeanjean <adrien.jeanjean@epsi.fr>
2021-11-04 10:37:05 +0100

    test

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\MessageGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +13,12 @@ class ServicesController extends AbstractController
     /**
      * @Route("/services", name="services")
      */
-    public function index(LoggerInterface $logger): Response
+    public function index(LoggerInterface $logger, MessageGenerator $messageGenerator): Response
     {
       // Créer un log pour les développeur symfony (dans admin : log)
-      $logger->info('Look, I just used a service!');
+      $message = $messageGenerator->getHappyMessage();
+      $logger->info($message);
+      $this->addFlash('success', $message);
 
         return $this->render('services/index.html.twig', [
             'controller_name' => 'ServicesController',
---
+++ src/Service/MessageGenerator.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Service;
+
+// Crée un service qui renvois un string
+
+class MessageGenerator
+{
+    public function getHappyMessage(): string
+    {
+        $messages = [
+            'You did it! You updated the system! Amazing!',
+            'That was one of the coolest updates I\'ve seen all day!',
+            'Great work! Keep going!',
+        ];
+
+        $index = array_rand($messages);
+
+        return $messages[$index];
+    }
+}
